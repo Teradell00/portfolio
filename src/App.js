78d9f08a66ecc930f8bce18ea16f2de2cb75b59c
MuhammadAnique2171 <l202171@lhr.nu.edu.pk>
2024-02-17 17:07:35 +0500

    Responsiveness

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <div className="h-[6000px] w-full bg-primary_black relative flex justify-center ">
-      <img src={shade} className=' -translate-y-64  w-[700px] opacity-30 absolute z-30' alt="" />
+      <img src={shade} className='-translate-y-[125px] translate-x-30 sm:-translate-y-64  w-[700px] opacity-20 sm:opacity-30 absolute z-30' alt="" />
       <Home/>   
     </div>
 
